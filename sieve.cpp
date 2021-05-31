f9374e59e8cc3b06c8493f5d4b7b6814509076d0
Aidan Beckley <aidanbeckley@gmail.com>
2021-05-31 14:50:01 -0400

    Optimized

--- sieve.cpp
+++ sieve.cpp
@@ -13,34 +13,36 @@ void printarray(int64_t* a, int64_t len) {
 	std::cout << std::endl;
 }
 
+inline int_fast64_t array_index(int_fast64_t k) { return (k % 2) * (k-1) / 2; }
+
 int main(int argc, char* argv[]) {
 
 	clock_t t0 = clock();
 	int_fast64_t N = 1000;
 	if (argc > 1) { N = std::stoi(argv[1]); }
 
-	int_fast64_t* nums = new int_fast64_t[N];
+	int_fast64_t arraylen = 2 + (N/2);
+	int_fast64_t* nums = new int_fast64_t[arraylen];
 	int_fast64_t i, j;
 
-	for (i = 0; i < N; i++) { nums[i] = 0; }
-	int_fast64_t x = 3;
-	nums[2] = 2;
+	for (i = 0; i < arraylen; i++) { nums[i] = 0; }
+	int_fast64_t x;
 
 	// Set the odd numbers below N
-	for (x = 3; x < N; x += 2) { nums[x] = x; }
+	for (x = 1; x < N/2; x++) { nums[x] = 2 * x + 1; }
 
+	//Remove multiples of x
 	x = 3;
-
-	// Remove multiples of x
 	while ( x <= N/3 ) 
 	{
 		int_fast64_t a = x;
-		i = x * x;
-		while (i < N) 
+		i = a * x;
+		j = (i-1)/2;
+		while (j < N/2) 
 		{
-			i = a * x;
-			if (i < N) { nums[i] = 0; }
+			nums[j] = 0;
 			a += 2;
+			j = (a * x - 1) / 2;
 		}
 
 		x += 2;
@@ -48,9 +50,9 @@ int main(int argc, char* argv[]) {
 
 	std::ofstream f;
 	f.open("primes.txt");
-	f << nums[2] << '\t';
+	f << 2 << '\t';
 
-	for (i = 3; i < N; i += 2) {
+	for (i = 1; i < N/2; i++) {
 		if (nums[i] > 0) { f << nums[i] << '\t'; }
 	}
 	f << std::endl;
