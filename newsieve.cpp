84f1caa322b0d23e57c98077516cc73f3a6dcce7
abeckley27 <aidanbeckley@gmail.com>
2023-12-26 22:20:14 -0500

    added comments

--- newsieve.cpp
+++ newsieve.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <cstdint>
 
+// I forget why this is here
 void printarray(int64_t* a, int64_t len) {
 	int_fast64_t i;
 	for (i = 0; i < len; i++) {
@@ -13,14 +14,18 @@ void printarray(int64_t* a, int64_t len) {
 	std::cout << std::endl;
 }
 
+// Find all prime numbers below N
 int main(int argc, char* argv[]) {
-	//Finds prime numbers below N
 
-	clock_t t0 = clock();
+	//Initialize
+	const clock_t t0 = clock();
 	int64_t N = 1000000;
+	int64_t i;
+
+	// Allow for the upper bound to be specified as an argument
 	if (argc > 1) { N = std::stol(argv[1]); }
 
-	int64_t i;
+	//bitmap for primality
 	bool* s = new bool[N+1];
 
 	// 0 and 1 are not prime
@@ -31,6 +36,7 @@ int main(int argc, char* argv[]) {
 
 	int64_t x;
 
+	//Remove multiples of primes
 	for (x = 2; x < ceil(sqrt(N)); x++) {
 		if (s[x]) {
 			for (int64_t multiple = x * x; multiple < N + 1; multiple += x) {
@@ -39,6 +45,9 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
+	clock_t t1 = clock();
+
+	// File output
 	std::ofstream f;
 	f.open("primes.txt");
 	int64_t count = 0;
@@ -52,11 +61,12 @@ int main(int argc, char* argv[]) {
 	f << std::endl;
 	f.close();
 
-	std::cout << "Pi(" << N << ") = " << count << std::endl;
-
-	clock_t t1 = clock();
-	std::cout << "Time: " << (double(t1) - double(t0)) / CLOCKS_PER_SEC << " s \n";
-
 	delete[] s;
+
+	std::cout << "Pi(" << N << ") = " << count << std::endl;
+	clock_t t2 = clock();
+	std::cout << "Calculation Time: " << (double(t1) - double(t0)) / CLOCKS_PER_SEC << " s \n";
+	std::cout << "Total Time: " << (double(t2) - double(t0)) / CLOCKS_PER_SEC << " s \n";
+
 	return 0;
 }
